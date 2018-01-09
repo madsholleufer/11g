90d8af58ce9f0c81cf996cde32fe91349029968e
Isabella <30567958+isabellaedwards@users.noreply.github.com>
2018-01-09 20:15:22 +0100

    Add files via upload

---
+++ testPiecesOnBoard.fsx
@@ -0,0 +1,21 @@
+// Test af funktionen piecesOnBoard. Vi ser om den returnerer en liste af chessPieces.
+open Chess
+open Pieces
+/// Print various information about a piece
+let printPiece (board : Board) (p : chessPiece) : unit =
+  printfn "%A: %A %A" p p.position (p.availableMoves board)
+
+// Create a game
+let board = Chess.Board () // Create a board
+// Pieces are kept in an array for easy testing
+let pieces = [|
+  king (White) :> chessPiece;
+  rook (White) :> chessPiece;
+  king (Black) :> chessPiece |]
+// Place pieces on the board
+board.[0,0] <- Some pieces.[0]
+board.[1,1] <- Some pieces.[1]
+board.[4,1] <- Some pieces.[2]
+
+// Make moves
+printfn "%A" board.piecesOnBoard
